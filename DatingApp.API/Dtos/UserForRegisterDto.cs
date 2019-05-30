8ff2ac9c3ec75839c151e203b9d80bd1fe55f573
Mrt Erg <muratergun3@gmail.com>
2019-05-31 01:02:07 +0300

    Authorization Done

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username{get;set;}
-
+        [Required]
+        [StringLength(8,MinimumLength=4,ErrorMessage="Specify password betwenn 4 and 8 char")]
         public string Password{get;set;}
     }
 }
