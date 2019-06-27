2b473547af821e393abaaba1416801b097772e60
Mrt Erg <muratergun3@gmail.com>
2019-06-27 22:56:29 +0300

    3rd party

--- DatingAppSPA/src/app/app.component.ts
+++ DatingAppSPA/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import { AuthService } from './_services/auth.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DatingAppSPA';
+  jwtHelper=new JwtHelperService();
+
+
+  constructor(private authService:AuthService){}
+
+  ngOnInit() {
+    const token=localStorage.getItem('token');
+    if(token){
+      this.authService.decodedToken=this.jwtHelper.decodeToken(token);
+    }
+    
+  }
+  
 }
